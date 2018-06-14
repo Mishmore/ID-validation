59f93bb06961b7692b20a82113a50953c3387f07
Mishmore <michellemore28@gmail.com>
2018-06-14 17:17:46 -0500

    json

--- js/components/rideList.js
+++ js/components/rideList.js
@@ -17,7 +17,7 @@ export default class RideList extends React.Component {
     }
 
     componentWillMount() {
-        getJSON('../data.json', (err, json) => {
+        getJSON('./data.json', (err, json) => {
             (err) ? alert(err.message) : this.setState({ rides: json.items });
         })
     }
@@ -26,27 +26,27 @@ export default class RideList extends React.Component {
         window.addEventListener('scroll', () => this.infiniteScroll());
         console.log('mounted');
     }
-    
+
     componentWillUpdate() {
-        this.state.loading ? 
+        this.state.loading ?
         setTimeout(() => {
-            getJSON('../data2.json', (err, json) => {
+            getJSON('./data2.json', (err, json) => {
                 (err) ? alert(err.message) : this.setState({ rides2: json.items });
                 this.setState({ loading: false });
-            }) 
+            })
         }, 500)
-        : false ;    
+        : false ;
     }
 
     componentWillUnmount() {
-        window.removeEventListener('scroll', () => this.infiniteScroll());      
+        window.removeEventListener('scroll', () => this.infiniteScroll());
     }
 
     infiniteScroll() {
         if (this.state.rides2 == null) {
             if ((window.innerHeight + window.scrollY) >= (document.body.offsetHeight - 200)) {
                 this.setState({ loading: true });
-            } else { return false }     
+            } else { return false }
         }
     }
 
@@ -54,13 +54,13 @@ export default class RideList extends React.Component {
         return(
             <ul className={styles.rideList}>
                 { this.state.rides ?
-                    this.state.rides.map(elm => 
-                    <Ride origin={elm.src_loc.address.address} destination={elm.dst_loc.address.address} total={`S/ ${elm.receipt.total.amount}`} rideinfo={elm}/>) 
+                    this.state.rides.map(elm =>
+                    <Ride origin={elm.src_loc.address.address} destination={elm.dst_loc.address.address} total={`S/ ${elm.receipt.total.amount}`} rideinfo={elm}/>)
                     : <div className={styles.loading}></div>
                 }
                 { this.state.rides2 ?
-                    this.state.rides2.map(elm => 
-                    <Ride origin={elm.src_loc.address.address} destination={elm.dst_loc.address.address} total={`S/ ${elm.receipt.total.amount}`} rideinfo={elm}/>) 
+                    this.state.rides2.map(elm =>
+                    <Ride origin={elm.src_loc.address.address} destination={elm.dst_loc.address.address} total={`S/ ${elm.receipt.total.amount}`} rideinfo={elm}/>)
                     : <div className={styles.loading}></div>
                 }
             </ul>
